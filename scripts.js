504a3fea121aa8429d26645d6fa3071148835c15
Glenn Langdon <glennlangdon@gmail.com>
2022-03-28 15:49:50 -0500

    board and keyboard js

---
+++ scripts.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    createBoard();
+    createKeyboard();
+
+    function createKeyboard() {
+        let letterArr = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
+        let secondRowBegin = letterArr.indexOf('p');
+        let thirdRowBegin = letterArr.indexOf('l');
+        for (ltr of letterArr) {
+            let boardKey = document.createElement('div');
+                boardKey.classList.add('board-key');
+                boardKey.innerHTML = ltr;
+            if (letterArr.indexOf(ltr) <= secondRowBegin) {
+                document.querySelector('#row1').append(boardKey);
+            } else if (letterArr.indexOf(ltr) <= thirdRowBegin) {
+                document.querySelector('#row2').append(boardKey);
+            } else {
+                document.querySelector('#row3').append(boardKey);
+            }
+        }
+    }
+
+    function createBoard() {
+        const boardWrapper = document.querySelector('#board-wrapper');
+        const numGuesses = 6;
+        const wordLength = 5;
+
+        for (let i = 1; i <= numGuesses; i++) {
+            var row = document.createElement('div');
+            row.classList.add('board-row');
+            for (let j = 0; j < wordLength; j++) {
+                var letterBox = document.createElement('div');
+                letterBox.classList.add('letter-box');
+                // set data attribute on individual cells per row 10,11, 12 ... 20, 21, 22...
+                letterBox.setAttribute("data-cell-num", "" + i + j);
+                row.append(letterBox);
+            }
+            boardWrapper.append(row);
+        }
+    }
+
+   
+
+    console.log('DOM content parsed and loaded');
+});
